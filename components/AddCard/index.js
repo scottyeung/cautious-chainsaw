00499e9abfb8c5e291acb238138d202452f2588a
scottyeung <micold@gmail.com>
2021-08-05 19:38:22 +0800

    Fix Next/Image overlay

--- components/AddCard/index.js
+++ components/AddCard/index.js
@@ -21,7 +21,7 @@ export default function AddCard({user, setUser, setNewUser}) {
 	return (
 		<div className={styles.card}>
 			<div className={styles.card__info}>
-				<Image 
+				<img 
 					className={styles.card__info_img}
           alt="avatar"
 					src={"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"}
